853a073b9851cedf5ade3e61eef5205a244c7065
Opocx <68042954+Opocx@users.noreply.github.com>
2024-01-07 20:41:44 +0800

    Update Fimo.js

--- JS/Fimo.js
+++ JS/Fimo.js
@@ -1,1 +1,1 @@
-var obj=JSON.parse($response.body);obj.subscribe={valid:!0,forever:1,endTime:4000000000},$done({body:JSON.stringify(obj)});
+var obj=JSON.parse($response.body);obj.subscribe={valid:!0,forever:1,endTime:4092599349},$done({body:JSON.stringify(obj)});
